9885bfc296250e11734ba6dd5c77be076f4baf33
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-16 08:46:56 -0600

    Variables

--- Lesson-01/main.py
+++ Lesson-01/main.py
@@ -2,3 +2,20 @@ print("Hello World from Python")
 
 # Sending greeting using my name
 print("Hello I am Jonatan")
+
+# Variables in Python
+print('----------------------------')
+print("Variables in Python")
+myVariable = "Hello from Python"
+print(myVariable)
+
+myVariable = 2
+print(myVariable)
+x = 9
+y = 10
+print(x+y)
+
+x = 3
+y = 7
+z = x + y
+print(z)
