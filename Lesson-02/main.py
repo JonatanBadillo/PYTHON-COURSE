ef4d3d58a1b1608cc6bbba8c38eece2ddff263a2
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-17 09:24:53 -0600

    Arithmetic operators

---
+++ Lesson-02/main.py
@@ -0,0 +1,24 @@
+# Arithmetic operators
+a = 3
+b = 2
+addition = a + b
+#print('The result is: ',addition)
+print(f'The result of the addition is: {addition}')
+
+substraction = a - b
+print(f'The result of the substraction is: {substraction}')
+
+multiplication = a * b
+print(f'The result of the multiplication is: {multiplication}')
+
+division = a / b
+print(f'The result of the division is: {division}')
+
+division = a // b
+print(f'The result of the division(int) is: {division}')
+
+residue = a % b
+print(f'The residue is: {residue}')
+
+exponent = a ** b
+print(f'The result of the exponent is: {exponent}')
