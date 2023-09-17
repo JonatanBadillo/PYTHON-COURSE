0f4d5cabb4993b59b037ecaca88ffb32905fda45
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-17 09:31:49 -0600

    Rectangle Exercise

--- Lesson-02/main.py
+++ Lesson-02/main.py
@@ -22,3 +22,34 @@ print(f'The residue is: {residue}')
 
 exponent = a ** b
 print(f'The result of the exponent is: {exponent}')
+
+
+# Exercise
+'''
+In the following exercise, we are asked to calculate the area and perimeter of a Rectangle. To do this, we will need to create the following variables:
+
+height (int)
+width (int)
+
+The user should provide the values for height and width,
+and then the result should be printed in the following format
+(no accents and respect spaces, capital letters, lowercase letters, and line breaks):
+
+Provide the height:
+Provide the width:
+Area: <area>
+Perimeter: <perimeter>
+
+The formulas for calculating the area and perimeter of a Rectangle are:
+Area: height * width
+Perimeter: (height + width) * 2
+'''
+print('----------------------------')
+print('Rectangle Exercise')
+height = int(input('Provide the height: '))
+width = int(input('Provide the width: '))
+area = height * width
+perimeter = (height + width) * 2
+
+print(f'Area: {area}')
+print(f'Perimeter: {perimeter}')
