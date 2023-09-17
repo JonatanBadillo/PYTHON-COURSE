f7d425760fee6748e539563d8453a990ac21db08
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-17 13:21:40 -0600

    If/Else

---
+++ Lesson-03/main.py
@@ -0,0 +1,25 @@
+# If/Else
+print('If/Else')
+condition = 'hola'
+
+if condition == True:
+    print('True Condition')
+elif condition == False:
+    print('False Condition')
+else:
+    print('Unrecognized Condition')
+
+# Converting numbers to text
+print('----------------------------')
+print('Converting numbers to text')
+
+number = int(input('Provide a number between 1 and 3: '))
+
+if number == 1 :
+    print('One')
+elif number == 2 :
+    print('Two')
+elif number == 3 :
+    print('Three')
+else:
+    print('Invalid Number')        
