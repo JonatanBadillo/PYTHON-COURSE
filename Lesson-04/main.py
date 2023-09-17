7f0a4cb26ae81c2b8942beca0e7fc0b259fcd02c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-17 13:54:10 -0600

    While Cycle

---
+++ Lesson-04/main.py
@@ -0,0 +1,15 @@
+# While
+print('While')
+
+# condition = True
+# while condition:
+#     print('Executing whyle cycle')
+# else:
+#     print('Ending of the whyle cycle')
+
+counter = 0
+while counter<3:
+    print(counter+1)
+    counter += 1
+else:
+    print('Ending of the whyle cycle')
