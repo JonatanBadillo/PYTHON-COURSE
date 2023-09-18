9e4d4128d7c601873280d6b7103e63835c8ee29e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-18 08:46:02 -0600

    CONTINUE

--- Lesson-04/main.py
+++ Lesson-04/main.py
@@ -48,3 +48,15 @@ for letter in 'Netherland':
 else:
     print('Ending of the for cycle')
 
+# CONTINUE
+print('----------------------------')
+print('CONTINUE')
+''' 
+for i in range(6):
+    if i % 2 == 0:
+        print(f'Value: {i}')
+'''
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f'Value: {i}')
